3d4e7d5d1e35bbbbe070e23ce11ad0e0b75558b2
22Isadeus <kauaisadeus@gmail.com>
2025-11-12 11:06:10 -0300

    aula1

---
+++ aula1.PY/Desktop/aula1.PY/variaveis.py
@@ -0,0 +1,50 @@
+# criando variaveis, para criar uma variavel devemos dar um nome para ela o que aquela variavel corresponde, diferente de outras linguagens de programação mpo pyton não é necessario passar o tipo de dados.
+#atençâo!
+#caso sua variavel tenha mais de duas palavras devemos seguir o padrão convencional utilizado pelos programadores, chamado de snake case.
+
+isadeus_kaua = 18
+#tipos primitivos 
+# Tipos primitivos (int, float, bool e str)
+# Funçao: type()
+
+#int()
+# O int() (inteiro) é um tipo composto por caracteres numéricos (algaritimos), utilizados para responder um numeroque pode ser escrito sem um componente decimal, podendo ter ou não sinal, isto é, ser positivo ou negativo. Os valores do tipo inteiro podem ter um tamanho ilimitado.
+
+#exemplo de int
+
+
+#float()
+#o float() (flutuante) tambem é composto pos caracteres numéricos, mas é utilizado para representar numeros racionais(numeros que podem ser escritos como frações), tambem conheccidos com ponto flutuante. Assim somo os inteiros, podem ter sinal ou não o possuem um tamanho ilimitado. as casas decimais são separadas por porção inteira por um ponto(.)
+
+#bool()
+#o bool() é umtipo de dado lógico que pode assumir apenas dois valores: falso ou verdadeiro (false or true, respectivamente em python). na lógica computacional, podem ser considerados como 0(falso) ou 1 (verdadeiro)
+
+#str
+#o str()(abreviação de string, que emm portugues podemos traduzir como cadeia de caracteres), é uma sequencia de caracters utilizada para representar palavras, frases ou textos. As strings podem ser declaradas de diferentes formas por meio de aspas simples('), aspas duplas ("") ou triplas de aspas(''')- três aspas simpls consecutivas.
+
+#função: type()
+#o python possui diferentes tipos de dados que classificam a categoria da informação que está sendo manipulada no código. entretanto, podem existir situação em que precisamos descobrir qual é o tipo de um determinado valor.
+
+#operacionais arimetricos
+#Simbolos
+#como utilizar 
+
+#o python possui seta operadores arimetricas que são utilizados para realizar operações matematicas como adição subtração e mutiplicação. são elas
+
+#  +   Adição             soma de dois valores                                                                            x  +  y
+#  -   subitração         subtrai o valor da esquerda pra direita                                                         x  -  y
+#  *   multiplicação      multiplica os dois valores                                                                      x  *  y
+#  /   divisão            divide o valor da esquerda para a direita. O resultado sempre retorna como float.               x  /  y
+#  %   modulo             resto da divisão do valor da esquerda pelo da direita. o resultado sempre retorna int.          x  %  y
+# / /  divisão inteira    do valor da esquerda pelo valor da direita. o resultado sempre é int.                           x / / y
+# * *  exponenciação      realiza a potenciaçlização do valor da esquerda elevando ele ao valor da direita.               x * * y
+
+
+#cada operador tem sua utilização dentro do necessario, no python quando for calcular algum valor ele usa a convensão tradicional da matematica muito conhecida como PENDAS.
+# P    PARENTESES 
+# E    EXPONENCIAÇAÕ 
+# M    MULTIPLICAÇÃO 
+# D    DIVISÃO 
+# A    ADIÇÃO 
+
+#
